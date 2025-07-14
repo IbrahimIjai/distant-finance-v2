d81dd9104f147d0e59b5b35077c052df05fe42cb
ibrahimsunday <ibrahimsundayijai@gmail.com>
2025-07-14 02:28:12 +0100

    auth set up

---
+++ apis/offchain-worker/src/lib/auth.ts
@@ -0,0 +1,24 @@
+import { betterAuth } from "better-auth";
+import { prisma } from "@distant/db";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { emailOTP } from "better-auth/plugins";
+
+export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET as string,
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  plugins: [
+    emailOTP({
+      async sendVerificationOTP({ email, otp, type }) {
+        console.log("Sending OTP to", email, otp, type);
+      },
+    }),
+  ],
+});
--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,5 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import pkg from "../../package.json";
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
