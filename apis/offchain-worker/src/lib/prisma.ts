08f65aedd9d6fdb06d0888cefd86ce9ff15cfcb3
ibrahimsunday <ibrahimsundayijai@gmail.com>
2025-10-01 14:07:57 +0100

    remove cloudfare worker, renaming to 0xmove

--- apis/offchain-worker/src/lib/prisma.ts
+++
@@ -1,6 +0,0 @@
-import {  PrismaClient } from "@distant/db";
-import { withAccelerate } from "@prisma/extension-accelerate";
-
-export const prisma = new PrismaClient({
-   datasourceUrl: process.env.BETTER_AUTH_SECRET,
- }).$extends(withAccelerate());
